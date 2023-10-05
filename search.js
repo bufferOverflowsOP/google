1f186df192f46624e27a45c35eb8d623368e1745
bufferOverflowsOP <143848655+bufferOverflowsOP@users.noreply.github.com>
2023-10-05 15:51:29 -0400

    Changed error with search.length

--- search.js
+++ search.js
@@ -4,12 +4,11 @@ $(document).on("keypress", "input", function(e){
     let search = $(this).val();
     if (!(search.slice(0, 8) == "https://" || search.slice(0, 7) == "http://")) {
       for (i in search) {
-        if (i == "." && i == search.length - 3) {
+        if (i == "." && i == search.length - 4) {
           search = "https://" + search;
-          console.log(search);
           window.open(search, "_blank");
         } else {
-          alert("Website not found at: 'https://" + search);
+          alert("Website not found at: 'https://" + search + "'");
         }
       }
     } else {
