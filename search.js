e6b2212ec5ccdef62fc1271bf930e657d84f3a36
bufferOverflowsOP <143848655+bufferOverflowsOP@users.noreply.github.com>
2023-10-05 14:28:20 -0400

    Update search.js

--- search.js
+++ search.js
@@ -1,12 +1,13 @@
 $(document).on("keypress", "input", function(e){
   if (e.which == 13) {
     let search = $(this).val();
-    if (!(search.slice(0, 8) == "https://" || search.slice(0, 7) == "http://") && ){
+    if (!(search.slice(0, 8) == "https://" || search.slice(0, 7) == "http://")){
       for (i in search) {
         if (i == "." && i == search.length - 3) {
           search = "https://" + search;
           window.open(search, "_blank);
-        }
+        } else {
+          alert("Website not found at: 'https://" + search);
       }
     } else {
       window.open(search, "_blank");
