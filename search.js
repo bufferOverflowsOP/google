9bbf92717d36f5d8d2b75e335d71877889b89efe
bufferOverflowsOP <143848655+bufferOverflowsOP@users.noreply.github.com>
2023-10-05 15:47:34 -0400

    Added testing logs

--- search.js
+++ search.js
@@ -2,16 +2,18 @@
 $(document).on("keypress", "input", function(e){
   if (e.which == 13) {
     let search = $(this).val();
-    if (!(search.slice(0, 8) == "https://" || search.slice(0, 7) == "http://")){
+    if (!(search.slice(0, 8) == "https://" || search.slice(0, 7) == "http://")) {
       for (i in search) {
         if (i == "." && i == search.length - 3) {
           search = "https://" + search;
+          console.log(search);
           window.open(search, "_blank");
         } else {
           alert("Website not found at: 'https://" + search);
+        }
       }
     } else {
       window.open(search, "_blank");
     }
-  }
+    }
 });
