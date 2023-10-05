6a7f3baccfeefd0b8bccaf7282117cb2e07f711e
bufferOverflowsOP <143848655+bufferOverflowsOP@users.noreply.github.com>
2023-10-05 14:13:29 -0400

    Update search.js

--- search.js
+++ search.js
@@ -1,10 +1,7 @@
 var results = document.getElementById("result");
 var display = document.getElementById("display-result");
 $(document).on("keypress", "input", function(e){
-  if(e.which == 13){
-      var inputVal = $(this).val();
-        results.toggle.classList("results-valid");
-        display.innerHTML = searchup.value;
-        alert("Search success!");
-      }
-  });
+  if (e.which == 13) {
+    window.open($(this).val(), "_blank");
+  }
+});
