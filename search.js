441c6a0dd539c995e8850f2ec4b498956f5bd15a
bufferOverflowsOP <143848655+bufferOverflowsOP@users.noreply.github.com>
2023-10-06 08:50:32 -0400

    Fixed length bug...

--- search.js
+++ search.js
@@ -4,7 +4,7 @@ $(document).on("keypress", "input", function(e){
     let search = $(this).val();
     if (!(search.slice(0, 8) == "https://" || search.slice(0, 7) == "http://")) {
       for (i in search) {
-        if (i == "." && i == search.length - 4) {
+        if (i == "." && i == search.length - 3) {
           search = "https://" + search;
           window.open(search, "_blank");
         }
