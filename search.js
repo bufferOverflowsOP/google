57e96889d5fee709855b80ceecc3fe0f80c7fe6d
bufferOverflowsOP <143848655+bufferOverflowsOP@users.noreply.github.com>
2023-10-06 08:52:54 -0400

    Fixed conditional bug

--- search.js
+++ search.js
@@ -3,8 +3,8 @@ $(document).on("keypress", "input", function(e){
   if (e.which == 13) {
     let search = $(this).val();
     if (!(search.slice(0, 8) == "https://" || search.slice(0, 7) == "http://")) {
-      for (i in search) {
-        if (i == "." && i == search.length - 3) {
+      for (let i = 0; i < search; i++) {
+        if (search[i] == "." && i == search.length - 4) {
           search = "https://" + search;
           window.open(search, "_blank");
         }
