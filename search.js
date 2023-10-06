71216e47d3e48a14a9c89ab062e94aefca371000
bufferOverflowsOP <143848655+bufferOverflowsOP@users.noreply.github.com>
2023-10-06 08:54:47 -0400

    Update search.js

--- search.js
+++ search.js
@@ -4,6 +4,7 @@ $(document).on("keypress", "input", function(e){
     let search = $(this).val();
     if (!(search.slice(0, 8) == "https://" || search.slice(0, 7) == "http://")) {
       for (let i = 0; i < search; i++) {
+        alert(i);
         if (search[i] == "." && i == search.length - 4) {
           search = "https://" + search;
           window.open(search, "_blank");
